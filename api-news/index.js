e396b6739d05b6ab99d7b104e1a6cc72f29ade42
fafaew <rafael.gomest@gmail.com>
2023-10-11 16:23:37 -0300

    refactor routes

--- api-news/index.js
+++ api-news/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express();
+
 const userRoute = require("./src/routes/user.route");
 
-app.use("/hello", userRoute)
+const port = 3000
+
+app.use(express.json());
+app.use("/user", userRoute)
 
 
-app.listen(3000)
+app.listen(port, () => console.log(`Servidor rodando na porta ${port}`))
--- api-news/src/controllers/user.controller.js
+++ api-news/src/controllers/user.controller.js
@@ -1,5 +1,19 @@
-const hello = (req, res) => {
-  res.send("Hello World")
+const create = (req, res) => {
+  const {name, username, email, password, avatar, background} = req.body;
+  if (!name || !username || !email || !password || !avatar || !background) {
+    res.status(400).send({message:"submit all fields for registration"});
+  }
+
+  res.status(201).send({
+    message: "user created succesfully",
+    user: {
+      name,
+      username,
+      email,
+      avatar,
+      background,
+    },
+  });
 };
 
-module.exports = {hello}
+module.exports = {create}
